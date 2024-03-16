8687173e0326b891954de7ecc1c8ba80759b10c1
Meghana <meghanachavan784@gmail.com>
2024-03-16 17:59:03 +0530

    Past Events Page, Custom Query Pagination

--- single-program.php
+++ single-program.php
@@ -15,21 +15,13 @@ while(have_posts()){
     <div class="container container--narrow page-section">
     <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?> ">
-          <i class="fa fa-home" aria-hidden="true"></i> Event home </a> 
+          <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?> ">
+          <i class="fa fa-home" aria-hidden="true"></i> All Programs </a> 
           <span class="metabox__main"><?php the_title(); ?></span>
         </p>
       </div>
 
         <div class="generic-content"><?php  the_content(); ?></div>
-<?php 
-
-$relatedPrograms = get_field('related_programs');
-foreach($relatedPrograms as $program){
-  echo get_the_title($program);
-}
-
-?>
 <?php }
 get_footer();
 ?>
